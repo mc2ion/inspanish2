240edef3e2c98fa32e72454fa95ccbadaacfa2e4
Marion <mc2.ions@gmail.com>
2014-10-13 13:42:10 -0500

    cambios
    a

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+include "app/config.php";
+include "app/detect.php";
+
+if ($page_name=='' || $page_name=='index.php') {
+	include $browser_t.'/index.php';
+	}
+elseif ($page_name=='about.php') {
+	include $browser_t.'/about.php';
+	}
+elseif ($page_name=='services.php') {
+	include $browser_t.'/services.php';
+	}
+elseif ($page_name=='contact.php') {
+	include $browser_t.'/contact.php';
+	}
+elseif ($page_name=='404.html') {
+	include $browser_t.'/404.html';
+	}
+elseif ($page_name=='contact-post.html') {
+	include 'app/contact.php';
+	}
+else
+	{
+		include $browser_t.'/404.html';
+	}
+?>
---
+++ web/common.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @author LeaseMove.ca
+ * @copyright 2014
+ */
+
+function my_header($section=''){
+    $out = '<div class="container">
+	<div class="row header">
+		<div class="logo navbar-left">
+			<h1><a href="index.php"><img class="img-logo" src="web/images/logo.png" alt="In Spanish"/> </a></h1>
+		</div>
+	</div>
+	<div class="row h_menu">
+		<nav class="navbar navbar-default navbar-left" role="navigation">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		    </div>
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav">';
+              $class = ""; if ($section == "index") $class = "class = 'active'";
+	          $out .= "<li {$class}><a href=\"index.php\">Home</a></li>";
+		      $class = ""; if ($section == "about") $class = "class = 'active'";
+		      $out .=  "<li {$class}><a href=\"about.php\">About Us</a></li>";
+		      $class = ""; if ($section == "services") $class = "class = 'active'";
+		      $out .=  "<li {$class}><a href=\"services.php\">Services</a></li>";
+		      $class = ""; if ($section == "contact") $class = "class = 'active'";
+              $out .=  "<li {$class}><a href=\"contact.php\">Contact Us</a></li>";
+		      $out .= '</ul>
+		    </div><!-- /.navbar-collapse -->
+		    <!-- start soc_icons -->
+		</nav>
+		<div class="soc_icons navbar-right">
+			<ul class="list-unstyled text-center">
+				<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+			</ul>	
+		</div>
+	</div>
+	<div class="clearfix"></div>
+</div>';
+    return $out;
+}
+
+?>
